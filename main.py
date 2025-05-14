135feddbe30ed8855a9743eb4dc33ca53593ad17
root <root@DESKTOP-TRJBLUD.>
2025-05-15 00:30:50 +0900

    v4.0

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router as api_router
 import logging
 
-# 로깅 설정
+# logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
 app.include_router(api_router)
 
-# CORS 설정
+# CORS
 origins = [
     "https://handledangerrequest-aoviljd5hq-du.a.run.app",
     "http://localhost:8000",
@@ -21,10 +21,10 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST"],
-    allow_headers=["*"],  # Authorization, Content-Type 등
+    allow_headers=["*"],  # Authorization, Content-Type etc.
 )
 
-# 테스트용 기본 경로
+# test
 @app.get("/")
 def root():
     return {"message": "SIREN API 서버 작동 중"}
